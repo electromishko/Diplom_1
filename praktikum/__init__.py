3e0d3904c42a82bed16846fac163a658368325e4
sekretarev_m <sekretarev_m@protei.ru>
2026-03-30 01:02:45 +0300

    praktikum/

---
+++ praktikum/__init__.py
@@ -0,0 +1,14 @@
+from .bun import Bun
+from .burger import Burger
+from .ingredient import Ingredient
+from .database import Database
+from .ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+__all__ = [
+    'Bun',
+    'Burger',
+    'Ingredient',
+    'Database',
+    'INGREDIENT_TYPE_SAUCE',
+    'INGREDIENT_TYPE_FILLING',
+]
